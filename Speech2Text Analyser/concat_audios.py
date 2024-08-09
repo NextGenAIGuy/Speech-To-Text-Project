224e3aec0c3013e99eac2c87224cfdabb5037e48
Sam <datascientist4uu@gmail.com>
2024-08-09 11:06:43 +0530

    Delete Speech2Text Analyser directory

--- Speech2Text Analyser/concat_audios.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-audio1 = AudioSegment.from_wav("audio_file\AI Advancement.wav")
-audio2 = AudioSegment.from_wav("audio_file\\record_out.wav")
-
-
-combined_audio = audio2 + audio1 
-
-combined_audio.export("audio_file/combined_audio.wav", format="wav")
